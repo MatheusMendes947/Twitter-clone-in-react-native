549387da805bb3f027a65bde24f8b02373eba2e4
matheus <matheusmm2019@gmail.com>
2022-03-11 19:28:54 -0300

    Detalhes adicionados

--- screens/routes.js
+++ screens/routes.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { Entypo } from '@expo/vector-icons'; 
 
+import { Feather } from '@expo/vector-icons';
+
 import Home from './home';
 import Search from './search';
 import Mensages from './mensages';
@@ -20,7 +22,7 @@ export default function Routes() {
       <Tab.Navigator
       screenOptions={{
             tabBarStyle: {
-                        backgroundColor:'#121212'
+                        backgroundColor:'#121213'
             },
             tabBarShowLabel: false,
             // headerShown: false,
@@ -36,23 +38,35 @@ export default function Routes() {
         options={{tabBarIcon:({size, color})=>(
             <Entypo name="home" size={size} color={color} />
         )
-      
       }}
         />
 
         <Tab.Screen 
         name="Search" 
         component={Search} 
+        options={{tabBarIcon:({size, color})=>(
+            <Feather name="search" size={size} color={color} />
+        )
+      }}
         />
 
         <Tab.Screen 
         name="Notifications" 
         component={Notifications} 
+        options={{tabBarIcon:({size, color})=>(
+            <Feather name="bell" size={size} color={color} />
+        )
+      }}
+
         />
 
         <Tab.Screen 
         name="Mensages"
         component={Mensages} 
+        options={{tabBarIcon:({size, color})=>(
+            <Feather name="mail" size={size} color={color} />
+        )
+      }}
         />
 
       </Tab.Navigator>
